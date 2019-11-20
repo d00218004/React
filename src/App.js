2783e3b636e2733de2c29d7aeeda6c7330912c88
Matthew Walller <d00218004@student.dkit.ie>
2019-11-20 16:13:12 +0000

    Installed React my-app setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Hello World!</h1>
+      <h3>Test Terminal Connection</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
